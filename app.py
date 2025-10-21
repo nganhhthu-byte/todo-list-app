ce1ee65bb637df2a6b1ca8bb48050520075d97d4
Ng Anh Thuw <nganhhthu1176@gmail.com>
2025-10-21 22:07:49 +0700

    add-task

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ if __name__ == "__main__":
   print("Chào mừng đến với ứng dụng To-Do List!")
   add_task("Học bài Git và GitHub")
   add_task("Làm bài tập thực hành ở nhà")
+# Liệt kê tất cả các công việc 
+tasks = []  # ví dụ: ["Học Git", "Làm bài tập"]
+def add_task(name: str):
+    tasks.append(name)
